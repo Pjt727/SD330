0bbc112bc29e32c90cd0ecb060d82868aa91f135
Patrick <76497944+Pjt727@users.noreply.github.com>
2024-04-16 14:59:37 -0400

    Fixed contents and added rest of requirements

--- js/filelist.js
+++ js/filelist.js
@@ -1,24 +1,31 @@
-async function githubFilelist(){
-            
+async function githubFilelist(folderPath){
     const links = document.getElementById('links');
-    const BASE_URL = "https://api.github.com/repos/pjt727/SD330/";
+    const CONTENTS_URL = `https://api.github.com/repos/pjt727/SD330/contents${folderPath}`;
+    const BASE_URL_LINKS = "https://pjt727.github.io/SD330/"
 
-    const response = await fetch(`${BASE_URL}contents`);
+
+    const response = await fetch(CONTENTS_URL);
     const data = await response.json();
     // loop through the list of files, set "file" to each file name
     for (let file of data) {
+        console.log(file)
+        if(file.type === "dir") {
+            console.log(file.path);
+            githubFilelist(`/${file.path}/`);
+            continue;
+        }
         fname = file.name;
         fpathRel = file.path;
-        fpath = `${BASE_URL}${fpathRel}`;
+        fpath = `${BASE_URL_LINKS}${fpathRel}`;
         newItem = document.createElement("li");
         newLink = document.createElement("a");
         newLink.href = fpath
-        newLink.innerHTML = fname;
+        newLink.innerText = fname;
         newItem.appendChild(newLink);
         links.appendChild(newItem);
     }
 }
 
-document.addEventListener('load', () => {
-    githubFilelist()
+document.addEventListener('DOMContentLoaded', () => {
+    githubFilelist("");
 })
